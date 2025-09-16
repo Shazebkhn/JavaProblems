37304ef06b7c3467ac0f874a1bca5e1886f88485
SHAZKHA <shazeb.khan@thoughtworks.com>
2025-09-16 18:00:12 +0530

    feat(methods): fix stock problems

--- src/main/java/org/shazeb/Arrays/RotatedSortedArrayMin.java
+++ src/main/java/org/shazeb/Arrays/RotatedSortedArrayMin.java
@@ -2,27 +2,28 @@ package org.shazeb.Arrays;
 
 public class RotatedSortedArrayMin {
     public static void main(String[] args) {
-        int[] num = {3,4,5,6,7,8,2};
+//        int[] num = {3,4,5,6,7,8,2};
+        int[] num = {3, 1, 2};
         System.out.println(minRotatedArray(num));
     }
 
 
     //binary search and use logic that first element< last element then rotated elements exists
-    static int minRotatedArray(int[] num){
-        int lo = 0, hi = num.length-1;
+    static int minRotatedArray(int[] num) {
+        int lo = 0, hi = num.length - 1;
 
-        if(num[lo]<=num[hi]) return num[lo];//rotated n time or contains 1 elementl
+        if (num[lo] <= num[hi]) return num[lo];//rotated n time or contains 1 elementl
 
-        while(hi-lo>=1){
-//            System.out.println( "lo: " + lo + "hi: " +hi);
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
 
-            int mid = lo + (hi-lo)/2;
-            if(num[lo]>num[mid]){
-//                System.out.println( "lo: " + lo + "mid: " +mid);
+            System.out.println("lo: " + lo + " hi: " + hi + " mid: " + mid);
+            System.out.println("lo val: " + num[lo] + " hi val: " + num[hi] + " mid: " + num[mid]);
+
+            if (num[mid] > num[hi]) {
+                lo = mid + 1;
+            } else {
                 hi = mid;
-            }
-            else{
-                lo = mid;
             }
         }
         return num[lo];
